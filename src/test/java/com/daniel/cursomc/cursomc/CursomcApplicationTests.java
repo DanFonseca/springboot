f29df4a00a9ca67d1c353d2bcf1ca7c9eb6f7247
danie <daniel.freitas.ms1@gmail.com>
2019-11-30 15:55:03 -0300

    Projeto Criado

---
+++ src/test/java/com/daniel/cursomc/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.cursomc.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
